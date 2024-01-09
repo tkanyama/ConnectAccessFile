2465283b49394767fcc54908e90caeea86a1430a
克典 白藤 <k.shirarara@outlook.jp>
2024-01-09 15:07:26 +0900

    プロジェクト ファイルを追加します。

---
+++ ConnectAccessFile/AccessAPI.vb
@@ -0,0 +1,66 @@
+﻿
+Imports System.Data
+Imports System.Data.OleDb
+'Imports System.Data
+'Imports System.Reflection.Metadata.Ecma335
+
+Public Class AccessAPI
+
+    Private _ConnectionString As String = "Provider = Microsoft.ACE.OLEDB.12.0;Data Source="
+    Private _FilePath As String
+
+    Public Property FilePath() As String
+        Get
+            Return _FilePath
+        End Get
+        Set(ByVal value As String)
+            _FilePath = value
+        End Set
+    End Property
+
+    Public Function GetTableData(ByVal sql As String) As DataTable
+        'SQL作成
+
+        If sql <> "" And _FilePath <> "" Then
+            Dim resultDt As New DataTable
+            'Dim sql = New System.Text.StringBuilder()
+            'sql.AppendLine("SELECT")
+            'sql.AppendLine("  *")
+            'sql.AppendLine("FROM T_HANTEI_RIREKI")
+            'sql.AppendLine(" WHERE JUTAKU_NO = '230898'")
+
+            'Access接続準備
+            Dim command As New OleDbCommand
+            Dim da As New OleDbDataAdapter
+            Dim cnAccess As OleDbConnection = New OleDbConnection
+            'cnAccess.ConnectionString = My.Settings.AccessCon
+            'cnAccess.ConnectionString = "Provider = Microsoft.ACE.OLEDB.12.0;Data Source=W:\判定管理_DATA.accdb"
+            cnAccess.ConnectionString = _ConnectionString + _FilePath
+
+            'Access接続開始
+            cnAccess.Open()
+
+            Try
+
+                command.Connection = cnAccess
+                command.CommandText = sql.ToString
+                da.SelectCommand = command
+
+                'SQL実行 結果をデータテーブルに格納
+                da.Fill(resultDt)
+
+
+            Catch ex As Exception
+                Throw
+            Finally
+                command.Dispose()
+                da.Dispose()
+                cnAccess.Close()
+                GetTableData = resultDt
+            End Try
+        Else
+            GetTableData = Nothing
+        End If
+    End Function
+
+End Class
---
+++ ConnectAccessFile/Program.vb
@@ -0,0 +1,61 @@
+Imports System
+Imports System.Data
+Imports System.Data.OleDb
+
+Module Program
+    Sub Main(args As String())
+        'Console.WriteLine("Hello World!")
+
+        Dim API1 As New AccessAPI
+        'API1.FilePath = "W:\”»’èŠÇ—_DATA.accdb"
+        Dim Path As String = args(0)
+        Dim gbrcNo As String = args(1)
+
+        API1.FilePath = Path
+
+        'Dim sql As String = "SELECT * FROM T_HANTEI_RIREKI WHERE JUTAKU_NO = '230898'"
+        Dim sql As String = "SELECT * FROM T_HANTEI_RIREKI WHERE JUTAKU_NO = '" + gbrcNo + "'"
+
+        Dim resultDt As DataTable = API1.GetTableData(sql)
+
+        With resultDt
+
+            Dim rn As Integer = .Rows.Count
+            If rn > 0 Then
+                Dim itemName As String() = {"JUTAKU_NO", "JUTAKU_NO_EDA", "MemberID", "HA_Data_KUBUN", "HA_Number", "HA_Select_Date", "HA_JISSI_Date"}
+                Dim n0 As Integer = itemName.Length
+                Dim Ans As String = ""
+                For j As Integer = 0 To n0 - 1
+                    Ans += itemName(j)
+                    If j < n0 - 1 Then
+                        Ans += ","
+                    Else
+                        Ans += vbCrLf
+                    End If
+                Next
+
+                For i As Integer = 0 To rn - 1
+                    'Ans += .Rows(i).Item("JUTAKU_NO").ToString + ","
+                    'Ans += .Rows(i).Item("JUTAKU_NO_EDA").ToString + ","
+                    'Ans += .Rows(i).Item("MemberID").ToString + ","
+                    'Ans += .Rows(i).Item("HA_Data_KUBUN").ToString + ","
+                    'Ans += .Rows(i).Item("HA_Number").ToString + ","
+                    'Ans += .Rows(i).Item("HA_Select_Date").ToString + ","
+                    'Ans += .Rows(i).Item("HA_JISSI_Date").ToString + vbCrLf
+                    For j As Integer = 0 To n0 - 1
+                        Ans += .Rows(i).Item(itemName(j)).ToString
+                        If j < n0 - 1 Then
+                            Ans += ","
+                        Else
+                            Ans += vbCrLf
+                        End If
+                    Next
+                Next
+                Console.WriteLine(Ans)
+            Else
+                Console.WriteLine("Nothing")
+            End If
+        End With
+
+    End Sub
+End Module
